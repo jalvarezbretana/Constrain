2357f363a1a4fbd23e191e2b785453b93bc302fc
Luis <jalvarezbretana@danielcastelao.org>
2022-09-26 10:02:17 +0200

    Acabado spread, spread inside, weighted y packed

--- app/src/main/java/com/example/constrain/MainActivity.kt
+++ app/src/main/java/com/example/constrain/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Comentario para añadir un commit de "arreglo de spread y spread inside"
     }
 }
